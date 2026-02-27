f31557e3b55f08770e57c4e83410c99572a93f1b
gitmetothemooon <satvik.rishu@gmail.com>
2026-02-27 09:59:23 +0530

    UC3:String Reversal

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,38 @@
+/**
+ * MAIN CLASS: PalindromeCheckerApp
+ * Use Case 3: Palindrome Check Using String Reverse
+ *
+ * Description:
+ * This class demonstrates palindrome validation
+ * by reversing the string and comparing it
+ * with the original value.
+ *
+ * Key Concepts Used:
+ *  - Loop (for loop)
+ *  - String Immutability
+ *  - String Concatenation (+)
+ *  - equals() Method
+ *
+ * @author Satvik Sharma
+ * @version 1.0
+ */
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args){
+
+        String word = "madam";
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+
+            reversed = reversed + word.charAt(i);
+        }
+
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a palindrome");
+        } else {
+            System.out.println(word + " is not a palindrome");
+        }
+    }
+}
